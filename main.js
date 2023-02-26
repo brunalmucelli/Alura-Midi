c7a8045e1e6fce957cccb5fc1f02a60323bca8c1
brunalmucelli <brunalmucelli@gmail.com>
2023-02-26 17:07:25 -0300

    modificação enter e espaço

--- main.js
+++ main.js
@@ -6,11 +6,21 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 for (let i = 0; i < listaDeTeclas.length; i++) {
     const tecla = listaDeTeclas[i];
-    const instrumento =tecla.classList[1];
+    const instrumento = tecla.classList[1];
 
     const idAudio = `#som_${instrumento}`;
 
-    tecla.onclick=function(){
+    tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space'||evento.code === 'Enter') {
+            listaDeTeclas[i].classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        listaDeTeclas[i].classList.remove('ativa');
     }
 }
