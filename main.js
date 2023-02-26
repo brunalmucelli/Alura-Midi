47887d56df4fac447a80050f921379dbd6546104
brunalmucelli <brunalmucelli@gmail.com>
2023-02-26 15:41:48 -0300

    associação audios  e teclas

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio,) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
+    const instrumento =tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick=function(){
+        tocaSom(idAudio);
+    }
+}
